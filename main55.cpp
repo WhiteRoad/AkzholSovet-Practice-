4ed930c8bfe59aa2d5513e25acfa0180396c2127
WhiteRoad <57897837+WhiteRoad@users.noreply.github.com>
2019-11-19 13:12:33 +0600

    Add files via upload

---
+++ main55.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+int count=0;
+int a[3];
+for (int i=0;i<3;i++){
+cin>>a[i];
+if (a[i]>0)
+    count++;}
+cout<<count;
+return 0;
+}
+
