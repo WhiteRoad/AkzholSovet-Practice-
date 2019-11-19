644f5a1dd9c00c0b418825c54e9596f9fc3c0a68
WhiteRoad <57897837+WhiteRoad@users.noreply.github.com>
2019-11-19 12:48:16 +0600

    Add files via upload

---
+++ main52.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+int a,b,c,d;
+cin>>a>>b>>c>>d;
+if (a>5 and b>5 and c%6==0 and d%3!=0){
+    cout <<"Yes";}
+else cout <<"No";
+return 0;
+}
+
