1984e20d6a93b87f922cd971b68604f7fb38e7b7
WhiteRoad <57897837+WhiteRoad@users.noreply.github.com>
2019-11-21 09:38:52 +0600

    Add files via upload

---
+++ main69.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+int a,b,c;
+cin>>a>>b>>c;
+if (a+b>c and a+c>b and c+b>a){
+    cout<<"yes"<<"\n";}
+else cout<<"no";
+return 0;
+}
