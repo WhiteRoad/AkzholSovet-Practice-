b9269607279901433d325cfc6ded81587b6a315d
WhiteRoad <57897837+WhiteRoad@users.noreply.github.com>
2019-11-21 10:57:59 +0600

    Update main90.cpp

--- main90.cpp
+++ main90.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-double s=0,a=3;
 int main(){
-	for(int i=a;i<=111;i=i+2){
-		s=s+cos((i/(i+2)));
-	}
-	cout<<s;
+double s=0,a=3;
+for(int i=a;i<=111;i=i+2){
+	s=s+cos((i/(i+2)));}
+cout<<s;
 }
